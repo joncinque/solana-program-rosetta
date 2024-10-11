f064cf020b2b192e1d720697636d4b1e1362f0a9
Jon C <me@jonc.dev>
2024-10-12 00:28:05 +0200

    zig: Improve transfer-lamports performance with new SDK version (#8)
    * transfer-lamports: Upgrade zig dep to faster version
    
    * Update deps

--- transfer-lamports/zig/main.zig
+++ transfer-lamports/zig/main.zig
@@ -2,13 +2,9 @@ const sol = @import("solana-program-sdk");
 
 export fn entrypoint(input: [*]u8) u64 {
     const context = sol.Context.load(input) catch return 1;
-    const accounts = context.loadRawAccounts(sol.allocator) catch return 1;
-    defer accounts.deinit();
-
-    const source = accounts.items[0];
-    const destination = accounts.items[1];
+    const source = context.accounts[0];
+    const destination = context.accounts[1];
     source.lamports().* -= 5;
     destination.lamports().* += 5;
-
     return 0;
 }
