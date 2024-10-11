e9ac891d386a1e761962f838fce2c5f23ce9b108
Jon C <me@jonc.dev>
2024-10-12 00:55:31 +0200

    cpi-zig: Update to newest SDK and SPL libs
    #### Problem
    
    The Zig CPI example uses a few too many compute units due to the
    overhead from entrypoint and CPI allocations, but the newest SDK and SPL
    libraries avoid allocations.
    
    #### Summary of changes
    
    Update the dependencies and use them, reducing CUs by over 300.

--- cpi/zig/main.zig
+++ cpi/zig/main.zig
@@ -6,10 +6,7 @@ const SIZE = 42;
 
 export fn entrypoint(input: [*]u8) u64 {
     const context = sol.Context.load(input) catch return 1;
-    const accounts = context.loadRawAccounts(sol.allocator) catch return 1;
-    defer accounts.deinit();
-
-    const allocated = accounts.items[0];
+    const allocated = context.accounts[0];
 
     const expected_allocated_key = sol.PublicKey.createProgramAddress(
         &.{ "You pass butter", &.{context.data[0]} },
@@ -20,12 +17,11 @@ export fn entrypoint(input: [*]u8) u64 {
     if (!allocated.id().equals(expected_allocated_key)) return 1;
 
     // Invoke the system program to allocate account data
-    system_ix.allocate(
-        sol.allocator,
-        allocated.info(),
-        SIZE,
-        .{ .seeds = &.{&.{ "You pass butter", &.{context.data[0]} }} },
-    ) catch return 1;
+    system_ix.allocate(.{
+        .account = allocated.info(),
+        .space = SIZE,
+        .seeds = &.{&.{ "You pass butter", &.{context.data[0]} }},
+    }) catch return 1;
 
     return 0;
 }
